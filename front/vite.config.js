f933c965d394fc6e9988fabbd9ae5e04014608f9
Tu Nombre <you@example.com>
2025-11-22 19:44:33 +0100

    implement DOM, Tailwind installation & style.css

---
+++ front/vite.config.js
@@ -0,0 +1,11 @@
+// This file configures Vite to automatically 
+// understand and compile Tailwind CSS
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
